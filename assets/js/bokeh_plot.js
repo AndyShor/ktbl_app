0425035b08779bfd48003803e88164d9525ca429
Andrey Shornikov <andrey.shornikov@eleqtron.com>
2024-10-24 13:37:20 +0200

    added js folder

---
+++ assets/js/bokeh_plot.js
@@ -0,0 +1,18 @@
+var source = new Bokeh.ColumnDataSource({
+    data: { x: [], y: [] }
+});
+
+var plot = Bokeh.Plotting.figure({
+    title: 'Example of random data',
+    tools: "pan,wheel_zoom,box_zoom,reset,save",
+    height: 300,
+    sizing_mode: 'scale_width',
+});
+
+// add a line with data from the source
+plot.line({ field: "x" }, { field: "y" }, {
+    source: source,
+    line_width: 2
+});
+
+Bokeh.Plotting.show(plot);
